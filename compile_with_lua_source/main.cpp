e59f1b4930d6b02e3c9c16586c60e29f454ce503
newk <newkedison@gmail.com>
2013-01-25 17:17:15 +0800

    Add a new demo for compile source code of lua with my own project Note: 1. See the Makefile in compile_with_lua_source, this demo will compile two target(linux and win) 2. To cross-compile a windows version, must install the mingw32 package by: $ sudo apt-get install mingw32

---
+++ compile_with_lua_source/main.cpp
@@ -0,0 +1,67 @@
+/*
+ * =============================================================================
+ *       Filename:  main.cpp
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  2013-01-01 22:00:01
+ *         Author:  newk (newk), newkedison@gmail.com
+ *        Company:  
+ *          Copyright (c) 2013, newk
+ * =============================================================================
+ */
+
+//Call a simple function(written by lua) from C++
+
+#include <stdio.h>
+#include <string.h>
+extern "C"
+{
+#include "liblua/lua.h"
+#include "liblua/lauxlib.h"
+#include "liblua/lualib.h"
+}
+
+int main(int argc, const char* argv[])
+{
+  //lua function define
+  const char* code = "function sum(a, b)\n"
+                     "  print('Lua:' .. a .. '+' .. b .. '=' .. a + b)\n"
+                     "  return a + b\n"
+                     "end";
+  int error;
+  lua_State *L = luaL_newstate();   /* opens Lua */
+  luaL_openlibs(L);  // open all lua libs
+  error = luaL_loadbuffer(L, code, strlen(code), "Sum");
+  if (error) 
+  {
+    fprintf(stderr, "luaL_loadbuffer:%s", lua_tostring(L, -1));
+    lua_pop(L, 1);  /* pop error message from the stack */
+    return -1;
+  }
+  //The lua function must compile first
+  error = lua_pcall(L, 0, 0, 0);
+  if (error)
+  {
+    fprintf(stderr, "lua_pcall_compile:%s\n", lua_tostring(L, -1));
+    lua_pop(L, 1);  /* pop error message from the stack */
+    return -1;
+  }
+  //Start to call the lua function
+  lua_getglobal(L, "sum"); //push function name to the statck
+  lua_pushinteger(L, 14);  //push first argument
+  lua_pushinteger(L, 33);  //push second argument
+  error = lua_pcall(L, 2, 1, 0); //call the lua function
+  if (error)
+  {
+    fprintf(stderr, "lua_pcall:%s\n", lua_tostring(L, -1));
+    lua_pop(L, 1);  /* pop error message from the stack */
+    return -1;
+  }
+  //Get the return value from stack, check if it is an integer and get it
+  int ret = (int)lua_tonumber(L, -1);
+  //Balance the stack
+  lua_pop(L, 1);
+  printf("Result: %d\n", ret);
+  lua_close(L);
+  return 0;
+}
